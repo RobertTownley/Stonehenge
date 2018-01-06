da5a1b9ea94f80bbc11be99eaf9fe3faea95c8ab
Robert Townley <Me@RobertTownley.com>
2018-01-06 11:30:04 -0500

    Newer version

--- stonehenge/cli_utils.py
+++ stonehenge/cli_utils.py
@@ -3,17 +3,17 @@ def print_usage(msg=None):
     if msg is not None:
         print(msg)
 
-    print('''\tWelcome to the Stonehenge project builder!\n
-            To use this utility, you'll first create a configuration file, and
-            then build the project based off of that configuration.\n\n
+    print("""Welcome to the Stonehenge project builder!
 
-            You can either create the config file (stonehenge_config.py)
-            yourself, or you can run "stonehenge new" to generate a file with
-            some helpful pre-populated defaults.\n
+To use this utility, you'll first create a configuration file, and
+then build the project based off of that configuration.
 
-            Once the file has been created, you can run "stonehenge build" to
-            create your project.\n
+You can either create the config file (stonehenge_config.py)
+yourself, or you can run "stonehenge new" to generate a file with
+some helpful pre-populated defaults.
 
-            For more information, or for usage examples, visit
-            https://github.com/RobertTownley/Stonehenge.
-    ''')
+Once the file has been created, you can run "stonehenge build" to
+create your project.
+
+For more information, or for usage examples, visit
+https://github.com/RobertTownley/Stonehenge.""")
