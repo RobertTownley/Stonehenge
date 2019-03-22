6d28975bcd32c807bd5f9eb8820805d1df816e7c
Robert Townley <me@roberttownley.com>
2019-03-22 16:21:03 -0400

    Initial components

---
+++ src/renderer/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>This is a dashboard</h2>
+</template>
+
+<script>
+import DefaultLayout from '@/layouts/DefaultLayout';
+
+export default {
+  extends: DefaultLayout
+}
+</script>
