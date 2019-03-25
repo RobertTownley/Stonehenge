17927dfa3ef2e3f17d38c452498de98d52869b8f
Robert Townley <me@roberttownley.com>
2019-03-25 17:15:37 -0400

    Stash

---
+++ src/router.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import DashboardView from './views/Dashboard.vue'
+import ApplicationView from './views/Application.vue'
+import MonitoringView from './views/Monitoring.vue'
+import SettingsView from './views/Settings.vue'
+import UsersView from './views/Users.vue'
+import AddServerView from './views/AddServer.vue'
+import EditServerView from './views/EditServer.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'dashboard',
+      component: DashboardView,
+    },
+    {
+      path: '/applications',
+      name: 'Applications',
+      component: ApplicationView,
+    },
+    {
+      path: '/monitoring',
+      name: 'Monitoring',
+      component: MonitoringView,
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: SettingsView,
+    },
+    {
+      path: '/users',
+      name: 'Users',
+      component: UsersView,
+    },
+    {
+      path: '/addserver',
+      name: 'Add Server',
+      component: AddServerView,
+    },
+    {
+      path: '/editserver',
+      name: 'Edit Server',
+      component: EditServerView,
+    },
+  ]
+})
