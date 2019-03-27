91eb152f2c358f7dc5b40d3fd02e0457dc578df8
Robert Townley <me@roberttownley.com>
2019-03-27 12:13:27 -0400

    hamburger icon

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,23 +1,71 @@
 <template>
   <div id='sidebar'>
-    <ServerList />
-    <Navigation />
+    <div v-if='activeServer' class='headerWrapper'>
+      <div class='header'>{{ activeServer.name }}</div>
+      <ToggleServerMenuButton :toggleMenu='toggleMenu'/>
+    </div>
+    <div v-else class='headerWrapper'>
+      <div class='header'>No Servers Found</div>
+      <ToggleServerMenuButton />
+    </div>
+    <div id="nav">
+      <router-link to="/applications">Applications</router-link>
+      <router-link to="/dashboard">Dashboard</router-link>
+      <router-link to="/monitoring">Monitoring</router-link>
+      <router-link to="/settings">Settings</router-link>
+      <router-link to="/Users">Users</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-import ServerList from '@/components/ServerList'
-import Navigation from '@/components/Navigation'
+import ToggleServerMenuButton from '@/components/buttons/ToggleServerMenu.vue'
 
 export default {
   components: {
-    Navigation,
-    ServerList,
-  }
+    ToggleServerMenuButton,
+  },
+  computed: {
+    activeServer() {
+      return this.servers.length ? this.servers[0] : null
+    },
+    serverCount() {
+      return this.servers.length
+    },
+  },
+  data() {
+    return {
+      servers: [{
+        name: 'Personal Server',
+      }],
+    }
+  },
+  methods: {
+    toggleMenu() {
+      console.log("TOGGLING");
+    }
+  },
 }
 </script>
 
-<style lang='scss'>
+<style lang='scss' scoped>
+@import '@/styles/_colors.scss';
+#nav {
+  padding: 16px;
+  a {
+    display: block;
+    width: 100%;
+    text-align: left;
+    font-size: 20px;
+    color: $darkblue;
+    padding: 8px 0px;
+    text-decoration: none;
+    outline: 0;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 #sidebar {
   border-right: 2px #888 solid;
   box-sizing: border-box;
@@ -25,5 +73,18 @@ export default {
   width: 320px;
   position: fixed;
   top: 0;
+}
+.headerWrapper {
+  background-color: $darkblue;
+  display: flex;
+  justify-content: space-between;
+  height: 50px;
+  .header {
+    color: white;
+    text-transform: uppercase;
+    text-align: left;
+    padding: 16px;
+    font-size: 24px;
+  }
 }
 </style>
---
+++ src/components/buttons/ToggleServerMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <button v-on:click='toggleMenu()'>
+    <img src='@/assets/icons/hamburgerWhite.svg'/>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['toggleMenu']
+}
+
+</script>
+
+<style lang='scss' scoped>
+@import '@/styles/_colors.scss';
+button {
+  background-color: $darkblue;
+  border: 0;
+  outline: 0;
+  width: 60px;
+}
+img {
+  height: 100%;
+}
+</style>
